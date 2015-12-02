c12e951417618caf09bf89a22665b7425ba5550e
984894 <984894@cs_labv47a-305.cs.mum.edu>
2015-12-02 15:35:30 -0600

    Adding simple validation behavior to BookController

--- src/controller/FormAddBookController.java
+++ src/controller/FormAddBookController.java
@@ -5,16 +5,16 @@
  */
 package controller;
 
-import controller.FormBaseController;
+import dao.BookDao;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
+import javafx.scene.control.Alert.AlertType;
+import model.Book;
 
 /**
  * FXML Controller class
@@ -37,17 +37,67 @@ public class FormAddBookController extends FormBaseController {
     @FXML
     private Button btnSave;
     
+    private List<String> invalidFields;//to add the invlaid fields
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {        
-        cmbCheckoutLength.getItems().addAll(7, 21); //possible values of the comboBox       
+        cmbCheckoutLength.getItems().addAll(7, 21); //possible values of the comboBox 
+        invalidFields = new ArrayList<>();
+        
+        //Validations
+        txtISBN.focusedProperty().addListener((arg0, oldValue, newValue) -> {
+        if (!newValue) { //when focus lost
+            String textValue = txtISBN.getText();
+            if(textValue.equals("") || !textValue.matches("\\d*") || textValue.length() != 13){
+                //when it not matches the pattern (1.0 - 6.0)
+                //set the textField empty
+                txtISBN.setText("Enter 13 digit number");
+                invalidFields.add("ISBN");
+            }
+            else
+                invalidFields.remove("ISBN");
+        }
+
+    });
+        
+        
     } 
     
-    @FXML    
-    private void btnSaveAction(ActionEvent event){
+    @FXML
+    private void btnSaveAction(ActionEvent event) {
         System.out.println("Implement Save");
+
+        if (!invalidFields.isEmpty()) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Following fields are invalid: ");
+            alert.setContentText(invalidFields.toString());
+            alert.showAndWait();
+            return;
+        }
+        
+        //if The data is valid
+        Book book = new Book();
+        book.setIsbn(txtISBN.getText());
+        book.setTitle(txtTitle.getText());
+        //bo
+        
+        BookDao bookDao = new BookDao();
+        Boolean saveResult = bookDao.addBook(book);
+        if (!saveResult) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Saving Book Failed");           
+            alert.showAndWait();
+            return;
+        }
     }
+    
+    
+    
+    
+           
 
 }
